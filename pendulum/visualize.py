a26b483000428b22466a9724bfca5e50e0086456
Naviary2 <infinitechess.org@gmail.com>
2026-02-18 21:31:03 -0700

    Prevent random movements

--- pendulum/visualize.py
+++ pendulum/visualize.py
@@ -66,10 +66,13 @@ def run(
                 obs, _ = env.reset()
 
         # --- action ---
-        if model is not None:
-            action, _ = model.predict(obs, deterministic=True)
-        else:
-            action = env.action_space.sample()
+        # Apply random force
+        # if model is not None:
+        #     action, _ = model.predict(obs, deterministic=True)
+        # else:
+        #     action = env.action_space.sample()
+        # Apply no force
+        action = np.array([0.0])
 
         obs, _reward, terminated, truncated, _ = env.step(action)
 
